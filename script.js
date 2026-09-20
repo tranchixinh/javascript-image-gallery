1dd9d3a9ca920821de6055b8d1f1623707562e0b
Trần Chi <24103163@st.phenikaa-uni.edu.vn>
2026-09-20 23:24:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
 function upDate(element) {
-    console.log("Mouse over");
-    console.log(element.alt);
-    console.log(element.src);
 
-    document.getElementById("image").innerHTML = element.alt;
+    console.log("Mouseover event triggered");
 
-    document.getElementById("image").style.backgroundImage =
-        "url('" + element.src + "')";
+    var image = document.getElementById("image");
+
+    image.style.backgroundImage = "url('" + element.src + "')";
+    image.innerHTML = element.alt;
 }
 
-function unDo() {
-    document.getElementById("image").style.backgroundImage = "url('')";
 
-    document.getElementById("image").innerHTML =
-        "Di chuột qua một hình ảnh bên dưới để hiển thị ở đây.";
+function unDo() {
+
+    console.log("Mouseleave event triggered");
+
+    var image = document.getElementById("image");
+
+    image.style.backgroundImage = "none";
+    image.innerHTML =
+        "Hover over an image below to display it here.";
+}
+
+
+function focusFunction(element) {
+
+    console.log("Focus event triggered");
+
+    var image = document.getElementById("image");
+
+    var img = element.querySelector("img");
+
+    image.style.backgroundImage =
+        "url('" + img.src + "')";
+
+    image.innerHTML = img.alt;
+}
+
+
+function blurFunction() {
+
+    console.log("Blur event triggered");
+
+    var image = document.getElementById("image");
+
+    image.style.backgroundImage = "none";
+    image.innerHTML =
+        "Hover over an image below to display it here.";
+}
+
+
+function addTabFocus() {
+
+    console.log("Page loaded - adding tabindex");
+
+    var figures = document.querySelectorAll("figure");
+
+    for (var i = 0; i < figures.length; i++) {
+
+        figures[i].setAttribute("tabindex", "0");
+
+        console.log(
+            "Added tabindex to figure " + (i + 1)
+        );
+    }
 }
